d2e7adac4527215d980213dc7d5820187a0d1c95
Gianluca Baio <g.baio@ucl.ac.uk>
2022-01-07 14:06:04 +0000

    first commit

---
+++ install.R
@@ -0,0 +1,5 @@
+install.packages("tidyverse")
+install.packages("rmarkdown")
+remotes::install_github("giabaio/bmhe_utils")  
+install.packages("INLA",repos=c(getOption("repos"),INLA="https://inla.r-inla-download.org/R/stable"), dep=TRUE)
+
